d5c23e8da6a5a36723cca91b7169c9438edf4e18
sbadran73 <76157715+sbadran73@users.noreply.github.com>
2023-08-07 10:18:16 +0300

    Updated Testing size to 25%

--- src/model/train.py
+++ src/model/train.py
@@ -38,7 +38,7 @@ def get_csvs_df(path):
 # TO DO: add function to split data
 def split_data(df):
     X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values    
-    return train_test_split(X, y, test_size=0.30, random_state=0)
+    return train_test_split(X, y, test_size=0.25, random_state=0)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
