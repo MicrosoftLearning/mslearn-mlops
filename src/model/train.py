d6b3e6d5b7c0c308d1e0be9609f80ebef0cd528a
Andre Heim <aheim001@hotmail.com>
2023-09-04 16:13:26 +0200

    comment removed

--- src/model/train.py
+++ src/model/train.py
@@ -34,7 +34,7 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+
 from sklearn.model_selection import train_test_split
 
  
