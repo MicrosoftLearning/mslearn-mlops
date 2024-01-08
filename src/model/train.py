dea0148d2fe9f9885a5b209bd2a837df16328ccb
Leonard Kunz <leonard.kunz@taod.de>
2024-01-08 09:42:15 +0100

    Incorrect specification in get csvs df function in train.py

--- src/model/train.py
+++ src/model/train.py
@@ -29,7 +29,7 @@ def main(args):
 def get_csvs_df(path):
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
+    csv_files = glob.glob(f"{path}")
     if not csv_files:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
